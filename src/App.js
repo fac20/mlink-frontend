df22483a98c063e58e2b8e2bc419ee9b1e822026
Amber <a.rignell16@ejm.org>
2020-10-22 12:02:40 +0100

    create react router
    import components into App.js
    
    create css variables for font and colours
    
    Co-authored-by: khadija-nur <59174800+khadija-nur@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import React from "react";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useLocation,
+  useHistory
+} from "react-router-dom";
+
+import Profile from "./components/Profile/Profile";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Network from "./components/Network/Network";
+import Onboarding from "./components/Onboarding/Onboarding";
+import EditProfile from "./components/Profile/EditProfile";
+import Requests from "./components/Requests/Requests";
+import Signup from "./components/SignupLogin/Signup";
+import Login from "./components/SignupLogin/Login";
 
 export default function App() {
-  return <h1>MLINK</h1>;
+  return (
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Onboarding />
+          </Route>
+          <Route path="/network" exact>
+            <Network />
+          </Route>
+          <Route path="/dashboard" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/profile/:name" exact>
+            <Profile />
+          </Route>
+          <Route path="/edit-profile" exact>
+            <EditProfile />
+          </Route>
+          <Route path="/requests" exact>
+            <Requests />
+          </Route>
+          <Route path="/signup" exact>
+            <Signup />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
