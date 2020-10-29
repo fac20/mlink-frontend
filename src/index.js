41042b39e195fa4633083ef870028f0a02a7207d
Amber <a.rignell16@ejm.org>
2020-10-29 09:51:25 +0000

    place auth0 domain and client id in .env file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+console.log(process.env.REACT_APP_AUTHO_CLIENT_ID);
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-t2n4419c.eu.auth0.com"
-    clientId="E0ifO9k3QWne73Yqsv1Bq6pBoCjd1rnV"
+    domain={process.env.REACT_APP_AUTHO_DOMAIN}
+    clientId={process.env.REACT_APP_AUTHO_CLIENT_ID}
     //redirect user after authentication
     redirectUri={"http://localhost:3000/setup"}
   >
-    <App />
+    <App />,
   </Auth0Provider>,
   document.getElementById("root")
 );
