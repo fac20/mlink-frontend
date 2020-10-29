0eaaa1f3d75f7cc656cba689fd25c5bf6a00f4eb
Amber <a.rignell16@ejm.org>
2020-10-29 12:19:27 +0000

    add audience and scope to use get silent access key

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,76 +1,129 @@
 import React from "react";
-import { queryGraphql } from "../../utils/SearchQueries.js";
-import {
-  UserProfile,
-  Button,
-  BlueBackArrow,
-  ProfileHeader,
-  ProfilePic,
-  Description,
-  Text,
-  EditIcon,
-  ReadMoreArrow,
-  Subtitle,
-  ProfileBody,
-  UserInfo,
-  Section
-} from "./Profile.styles";
+import { useAuth0 } from "@auth0/auth0-react";
 
-// current job
-// previous job
-// medical school
-// postgr
+const Profile = () => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = React.useState(null);
 
-export default function Profile() {
-  const [readMore, setReadMore] = React.useState(false);
-  // create a button
-  // when the button is clicked then it sets readmore to true
-  // when readmore is true
-  // then more info shows up and vise versa
+  React.useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-t2n4419c.eu.auth0.com";
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user"
+        });
+
+        console.log(accessToken);
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, []);
 
   return (
-    <>
-      <UserProfile>
-        <ProfileHeader>
-          <Button>
-            <BlueBackArrow size="37" />
-          </Button>
-          <ProfilePic size="97" />
-          <Description>
-            <Text></Text>
-          </Description>
-          <EditIcon size="27" />
-        </ProfileHeader>
-
-        <ProfileBody>
-          <UserInfo>
-            <Section>
-              <Subtitle>Medical school</Subtitle>
-              <Button onClick={() => setReadMore(!readMore)}>
-                <ReadMoreArrow size="32" readMore={readMore} />
-              </Button>
-            </Section>
-
-            <Text>lorem ipsum {readMore ? "hello" : "bye"}</Text>
-            <Text>lorem ipsum time time</Text>
-            {readMore && (
-              <>
-                <Text>lorem ipsum</Text>
-                <Text>lorem ipsum time time</Text>
-              </>
-            )}
-          </UserInfo>
-        </ProfileBody>
-      </UserProfile>
-
-      {/* <select>
-        {jobTitles.map((x) => {
-          return <option>{x}</option>;
-        })}
-      </select> */}
-    </>
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.accessToken} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? <pre>{JSON.stringify(userMetadata, null, 2)}</pre> : "No user metadata defined"}
+      </div>
+    )
   );
-}
+};
+
+export default Profile;
+
+// import React from "react";
+// import { queryGraphql } from "../../utils/SearchQueries.js";
+// import {
+//   UserProfile,
+//   Button,
+//   BlueBackArrow,
+//   ProfileHeader,
+//   ProfilePic,
+//   Description,
+//   Text,
+//   EditIcon,
+//   ReadMoreArrow,
+//   Subtitle,
+//   ProfileBody,
+//   UserInfo,
+//   Section
+// } from "./Profile.styles";
+
+// // current job
+// // previous job
+// // medical school
+// // postgr
+
+// export default function Profile() {
+//   const [readMore, setReadMore] = React.useState(false);
+//   // create a button
+//   // when the button is clicked then it sets readmore to true
+//   // when readmore is true
+//   // then more info shows up and vise versa
+
+//   return (
+//     <>
+//       <UserProfile>
+//         <ProfileHeader>
+//           <Button>
+//             <BlueBackArrow size="37" />
+//           </Button>
+//           <ProfilePic size="97" />
+//           <Description>
+//             <Text></Text>
+//           </Description>
+//           <EditIcon size="27" />
+//         </ProfileHeader>
+
+//         <ProfileBody>
+//           <UserInfo>
+//             <Section>
+//               <Subtitle>Medical school</Subtitle>
+//               <Button onClick={() => setReadMore(!readMore)}>
+//                 <ReadMoreArrow size="32" readMore={readMore} />
+//               </Button>
+//             </Section>
+
+//             <Text>lorem ipsum {readMore ? "hello" : "bye"}</Text>
+//             <Text>lorem ipsum time time</Text>
+//             {readMore && (
+//               <>
+//                 <Text>lorem ipsum</Text>
+//                 <Text>lorem ipsum time time</Text>
+//               </>
+//             )}
+//           </UserInfo>
+//         </ProfileBody>
+//       </UserProfile>
+
+//       {/* <select>
+//         {jobTitles.map((x) => {
+//           return <option>{x}</option>;
+//         })}
+//       </select> */}
+//     </>
+//   );
+// }
 
 // const [jobTitles, setJobTitles] = React.useState("");
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// console.log(process.env.REACT_APP_AUTHO_CLIENT_ID);
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain={process.env.REACT_APP_AUTHO_DOMAIN}
+//     clientId={process.env.REACT_APP_AUTHO_CLIENT_ID}
+//     audience="https://dev-t2n4419c.eu.auth0.com/api/v2/"
+//     scope="read:current_user update:current_user_metadata"
+//     //redirect user after authentication
+//     redirectUri={"http://localhost:3000/setup"}
+//   >
+//     <App />,
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
-import "./index.css";
 
-const dotenv = require("dotenv");
-dotenv.config();
-
-console.log(process.env.REACT_APP_AUTHO_CLIENT_ID);
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTHO_DOMAIN}
-    clientId={process.env.REACT_APP_AUTHO_CLIENT_ID}
-    //redirect user after authentication
-    redirectUri={"http://localhost:3000/setup"}
+    domain="dev-t2n4419c.eu.auth0.com"
+    clientId="E0ifO9k3QWne73Yqsv1Bq6pBoCjd1rnV"
+    redirectUri={"http://localhost:3000/profile"}
+    audience="https://dev-t2n4419c.eu.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
   >
-    <App />,
+    <App />
   </Auth0Provider>,
   document.getElementById("root")
 );
