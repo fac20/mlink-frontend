5e1ebe26432df7ba17046a491378e3b8317b6ea1
izaakrogan <izaakrogan@googlemail.com>
2020-10-29 14:41:29 +0000

    netflify

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { queryGraphql } from "../../utils/SearchQueries.js";
+
 import {
   UserProfile,
   Button,
@@ -16,17 +16,8 @@ import {
   Section
 } from "./Profile.styles";
 
-// current job
-// previous job
-// medical school
-// postgrad exams
-
 export default function Profile() {
   const [readMore, setReadMore] = React.useState(false);
-  // create a button
-  // when the button is clicked then it sets readmore to true
-  // when readmore is true
-  // then more info shows up and vise versa
 
   return (
     <>
@@ -41,7 +32,6 @@ export default function Profile() {
           </Description>
           <EditIcon size="27" />
         </ProfileHeader>
-
         <ProfileBody>
           <UserInfo>
             <Section>
@@ -50,7 +40,6 @@ export default function Profile() {
                 <ReadMoreArrow size="32" readMore={readMore} />
               </Button>
             </Section>
-
             <Text>lorem ipsum {readMore ? "hello" : "bye"}</Text>
             <Text>lorem ipsum time time</Text>
             {readMore && (
@@ -62,43 +51,6 @@ export default function Profile() {
           </UserInfo>
         </ProfileBody>
       </UserProfile>
-
-      {/* <select>
-        {jobTitles.map((x) => {
-          return <option>{x}</option>;
-        })}
-      </select> */}
     </>
   );
 }
-
-// const [jobTitles, setJobTitles] = React.useState("");
-
-// React.useEffect(() => {
-//   const query = `query GetUser {
-//       users(where: {full_name: {_eq: "Jane Doe"}}) {
-//         id
-//         bio
-//         current_job_speciality
-//         current_job_start
-//         current_job_title
-//         current_job_workplace
-//         current_location
-//         degree_end
-//         degree_start
-//         degree_title
-//         email
-//         full_name
-//         gmc_number
-//         medical_school
-//         open_to_collaboration
-//         open_to_giving_advice
-//         phone_number
-//         titles_list {
-//           title
-//         }
-//       }
-//     }`;
-
-//   queryGraphql(query).then((result) => console.log(result.data.users));
-// }, []);
