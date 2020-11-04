b9f6f4c7c836769b2babf5853818f62532098b69
khadija <59174800+khadija-nur@users.noreply.github.com>
2020-11-04 13:54:43 +0000

    style dashboard page as per the prototype

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import dashboardQueries from "../../utils/dashboardQueries";
-import { ColumnSection, Count, RowSection, Subtitle, DashboardBody } from "./Dashboard.styles";
-import { DotDiv, Loader } from "./Dashboard.styles";
-import Navigation from "../Navigation/Navigation.jsx";
+import {
+  ColumnSection,
+  Count,
+  CountContainer,
+  RowSection,
+  Subtitle,
+  DashboardBody,
+  DotDiv,
+  Loader
+} from "./Dashboard.styles";
 import profileIcon from "../../assets/images/profileicon.svg";
-import requestIcon from "../../assets/images/requesticon.svg";
+import Navigation from "../Navigation/Navigation.jsx";
 import { ProfileImg, RequestImg } from "../Network/Network.styles.jsx";
+import requestIcon from "../../assets/images/requesticon.svg";
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
@@ -31,33 +39,43 @@ export default function Dashboard() {
   console.log(typeof totalCount);
 
   return (
-    <DashboardBody>
+  <>
       <div>
         <RequestImg alt="request" src={requestIcon} />
         <Link to="/profile">
           <ProfileImg alt="profile" src={profileIcon} />
         </Link>
       </div>
-      <Subtitle>You have {totalCount ? totalCount.length : ""} existing mlinks</Subtitle>
-      <ColumnSection>
-        <RowSection>
-          <span>Members that attended the same medical School</span>
-          <Count>{totalCount ? totalCount.sameMedSchool.length : ""}</Count>
-        </RowSection>
-        <RowSection>
-          <span>Members that have the same specialisation</span>
-          <Count>{totalCount ? totalCount.sameSpecialisation.length : ""}</Count>
-        </RowSection>
-        <RowSection>
-          <span>Members that work in the same workplace</span>
-          <Count>{totalCount ? totalCount.sameWorkplace.length : ""}</Count>
-        </RowSection>
-        <RowSection>
-          <span>Members that work in the same city</span>
-          <Count>{totalCount ? totalCount.sameCity.length : ""}</Count>
-        </RowSection>
-      </ColumnSection>
-      <Navigation />
-    </DashboardBody>
+      <DashboardBody>
+        <Subtitle>You have {totalCount ? totalCount.length : ""} existing mlinks</Subtitle>
+        <ColumnSection>
+          <RowSection>
+            <span>Members that attended the same medical School</span>
+            <CountContainer>
+              <Count>{totalCount ? totalCount.sameMedSchool.length : ""}</Count>
+            </CountContainer>
+          </RowSection>
+          <RowSection>
+            <span>Members that have the same specialisation</span>
+            <CountContainer>
+              <Count>{totalCount ? totalCount.sameSpecialisation.length : ""}</Count>
+            </CountContainer>
+          </RowSection>
+          <RowSection>
+            <span>Members that work in the same workplace</span>
+            <CountContainer>
+              <Count>{totalCount ? totalCount.sameWorkplace.length : ""}</Count>
+            </CountContainer>
+          </RowSection>
+          <RowSection>
+            <span>Members that work in the same city</span>
+            <CountContainer>
+              <Count>{totalCount ? totalCount.sameCity.length : ""}</Count>
+            </CountContainer>
+          </RowSection>
+        </ColumnSection>
+        <Navigation />
+      </DashboardBody>
+    </>
   );
 }
